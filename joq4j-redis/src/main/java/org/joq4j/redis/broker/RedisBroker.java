efb73ea4a3b2ed47bd20615d01c9dc843eeef4f7
kumin <hoangminh.it.hut@gmail.com>
2020-04-29 23:52:31 +0700

    + redis connector

--- joq4j-redis/src/main/java/org/joq4j/redis/broker/RedisBroker.java
+++ joq4j-redis/src/main/java/org/joq4j/redis/broker/RedisBroker.java
@@ -1,11 +1,18 @@
-package org.joq4j.redis;
+package org.joq4j.redis.broker;
 
 import org.joq4j.broker.Broker;
 import org.joq4j.broker.Subscriber;
+import org.joq4j.redis.RedisConf;
+import org.joq4j.redis.connection.RedisConnectionProviders;
 import redis.clients.jedis.Jedis;
 
 public class RedisBroker implements Broker {
     private Jedis jedis;
+
+    public RedisBroker() {
+        this.jedis = RedisConnectionProviders.getDefault(new RedisConf());
+    }
+
     @Override
     public void subscribe(Subscriber subscriber, String... channels) {
 
@@ -23,12 +30,12 @@ public class RedisBroker implements Broker {
 
     @Override
     public void push(String queue, String... values) {
-
+        jedis.rpush(queue, values);
     }
 
     @Override
     public String pop(String queue) {
-        return null;
+        return jedis.rpop(queue);
     }
 
     @Override

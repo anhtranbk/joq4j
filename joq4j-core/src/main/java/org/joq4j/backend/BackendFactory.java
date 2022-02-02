7a585d59b9dac5524cd5f9e81a289e87f4620c1c
tjeubaoit <anhtn.bk@gmail.com>
2022-02-02 19:32:10 +0700

    Add BackendFactory from common URL

---
+++ joq4j-core/src/main/java/org/joq4j/backend/BackendFactory.java
@@ -0,0 +1,36 @@
+package org.joq4j.backend;
+
+import org.joq4j.Joq4jException;
+import org.joq4j.common.net.ConnectionUrl;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BackendFactory {
+
+    private static final Map<String, Class<? extends StorageBackend>> backendRegistry = new HashMap<>();
+
+    public static StorageBackend fromUrl(String url) {
+        ConnectionUrl connectionUrl = ConnectionUrl.parseFromString(url);
+        Class<? extends StorageBackend> clazz = backendRegistry.get(connectionUrl.scheme());
+        if (clazz == null) {
+            throw new IllegalArgumentException("Scheme not found " + connectionUrl.scheme());
+        }
+
+        try {
+            Constructor<? extends StorageBackend> constructor = clazz.getConstructor(String.class);
+            return constructor.newInstance(url);
+        } catch (NoSuchMethodException
+                | InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException e) {
+            throw new Joq4jException(e);
+        }
+    }
+
+    public static void addRegistry(String scheme, Class<? extends StorageBackend> clazz) {
+        backendRegistry.put(scheme, clazz);
+    }
+}

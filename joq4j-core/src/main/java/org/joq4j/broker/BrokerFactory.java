33c7f3f91d7d13e0abfe8501739498628a839bae
tjeubaoit <anhtn.bk@gmail.com>
2022-02-02 19:42:05 +0700

    Add BrokerFactory

--- joq4j-core/src/main/java/org/joq4j/broker/BrokerFactory.java
+++ joq4j-core/src/main/java/org/joq4j/broker/BrokerFactory.java
@@ -1,8 +1,36 @@
 package org.joq4j.broker;
 
+import org.joq4j.Joq4jException;
+import org.joq4j.common.net.ConnectionUrl;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BrokerFactory {
 
+    private static final Map<String, Class<? extends Broker>> brokerRegistry = new HashMap<>();
+
     public static Broker fromUrl(String url) {
-        return null;
+        ConnectionUrl connectionUrl = ConnectionUrl.parseFromString(url);
+        Class<? extends Broker> clazz = brokerRegistry.get(connectionUrl.scheme());
+        if (clazz == null) {
+            throw new IllegalArgumentException("Scheme not found " + connectionUrl.scheme());
+        }
+
+        try {
+            Constructor<? extends Broker> constructor = clazz.getConstructor(String.class);
+            return constructor.newInstance(url);
+        } catch (NoSuchMethodException
+                | InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException e) {
+            throw new Joq4jException(e);
+        }
+    }
+
+    public static void addRegistry(String scheme, Class<? extends Broker> clazz) {
+        brokerRegistry.put(scheme, clazz);
     }
 }

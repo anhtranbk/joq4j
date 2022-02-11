d77151ed17276df05e01fe2a6c45d917c3a6552b
tjeubaoit <anhtn.bk@gmail.com>
2022-02-11 21:42:05 +0700

    [examples] Prepare for multi examples. Move demo app into playground sub module

---
+++ examples/playground/src/main/java/org/joq4j/examples/playground/PlaygroundDemoApp.java
@@ -0,0 +1,97 @@
+package org.joq4j.examples.playground;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.joq4j.AsyncResult;
+import org.joq4j.Job;
+import org.joq4j.JobBuilder;
+import org.joq4j.JobQueue;
+import org.joq4j.backend.StorageBackend;
+import org.joq4j.common.utils.Threads;
+import org.joq4j.redis.jedis.RedisBackendFactory;
+import org.joq4j.redis.jedis.RedisBrokerFactory;
+import org.joq4j.serde.JacksonMessageEncoder;
+import org.joq4j.serde.MessageEncoder;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class PlaygroundDemoApp {
+
+    public static void main(String[] args) throws Exception {
+        Class.forName(RedisBrokerFactory.class.getName());
+        Class.forName(RedisBackendFactory.class.getName());
+
+        String redisUrl = "redis://localhost:6379/0";
+        JobQueue queue = JobQueue.builder()
+                .broker(redisUrl)
+                .backend(redisUrl)
+                .build();
+
+        loopProduceConsumeJob(queue);
+//        executeJobAndGetResult(queue);
+//        testCustomSerializer();
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    static void loopProduceConsumeJob(JobQueue queue) throws InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            Random random = new Random();
+            for (int i = 0; i < 100; i++) {
+                Threads.sleepSeconds(2);
+                final int a = random.nextInt(100);
+                final int b = random.nextInt(100);
+                queue.enqueue(() -> a + b);
+            }
+        });
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.HOURS);
+    }
+
+    static void executeJobAndGetResult(JobQueue queue) throws Exception {
+        StorageBackend backend = queue.backend();
+        AsyncResult result = queue.enqueue(() -> "Hello, World!");
+
+        String jobId = result.getJobId();
+        Job job = backend.fetchJob(jobId);
+
+        ObjectMapper om = JacksonMessageEncoder.currentObjectMapper();
+        om.enable(SerializationFeature.INDENT_OUTPUT);
+        System.out.println("Job fetched\n" + om.writeValueAsString(job));
+
+        while (!result.isDone()) {
+            Threads.sleepSeconds(2);
+            System.out.println("Job State:" + backend.getState(jobId));
+        }
+        System.out.println(result.get());
+    }
+
+    static void testCustomSerializer() {
+        Job job = Job.builder()
+                .task(() -> "Hello, sieunhan!")
+                .name("test custom serializer")
+                .id(UUID.randomUUID().toString())
+                .build();
+        MessageEncoder encoder = new JacksonMessageEncoder();
+        String encoded = encoder.writeAsString(job);
+        System.out.println(encoded);
+
+        try {
+            job = encoder.read(encoded, JobBuilder.class).build();
+            Object result = job.task().call();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void testProcessExecutor() {
+        CommandLineParser parser = new DefaultParser();
+    }
+}
---
+++ examples/playground/src/main/java/org/joq4j/examples/playground/SimpleWorker.java
@@ -0,0 +1,41 @@
+package org.joq4j.examples.playground;
+
+import org.joq4j.Job;
+import org.joq4j.JobQueue;
+import org.joq4j.common.utils.Threads;
+import org.joq4j.redis.jedis.RedisBackendFactory;
+import org.joq4j.redis.jedis.RedisBrokerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleWorker {
+
+    static final Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    public static void main(String[] args) throws Exception {
+        Class.forName(RedisBrokerFactory.class.getName());
+        Class.forName(RedisBackendFactory.class.getName());
+
+        String redisUrl = "redis://localhost:6379/0";
+        JobQueue queue = JobQueue.builder()
+                .broker(redisUrl)
+                .backend(redisUrl)
+                .build();
+        logger.info("Waiting for message...");
+
+        int counter = 0;
+        while (counter < 60) {
+            Job job = queue.pop("simple", 100);
+            if (job != null) {
+                counter = 0;
+                logger.info("Received new job: " + job);
+                Object result = job.task().call();
+                logger.info("Processed job: " + job.id() + ". Result: " + result);
+            } else {
+                Threads.sleepSeconds(1);
+                counter++;
+            }
+        }
+        System.out.println("Wait too long for new job. Exiting...");
+    }
+}

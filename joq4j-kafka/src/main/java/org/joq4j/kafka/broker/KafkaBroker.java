7c96cf2e36f1e1bcefbb41bb4fa89fa3c29297a1
kumin242 <minh.hoang4@tiki.vn>
2022-02-26 16:42:45 +0700

    feat: kafka broker

--- joq4j-kafka/src/main/java/org/joq4j/kafka/broker/KafkaBroker.java
+++ joq4j-kafka/src/main/java/org/joq4j/kafka/broker/KafkaBroker.java
@@ -1,12 +1,15 @@
 package org.joq4j.kafka.broker;
 
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.internals.KafkaConsumerMetrics;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.joq4j.broker.Broker;
 import org.joq4j.broker.Subscriber;
 import org.joq4j.config.Config;
 import org.joq4j.kafka.jafka.KafkaConfig;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 public class KafkaBroker implements Broker {
     private KafkaProducer<String, byte[]> kafkaProducer;
@@ -19,7 +22,7 @@ public class KafkaBroker implements Broker {
     }
     @Override
     public void subscribe(Subscriber subscriber, String... channels) {
-
+        this.kafkaConsumer.subscribe(Arrays.asList(channels));
     }
 
     @Override
@@ -39,11 +42,33 @@ public class KafkaBroker implements Broker {
 
     @Override
     public String pop(String queue) {
+        ConsumerRecords<String, byte[]> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+
         return null;
     }
 
     @Override
     public void close() {
+        kafkaConsumer.close();
+        kafkaProducer.close();
+    }
 
+    private static class MyKafkaSubscriber implements Subscriber {
+
+
+        @Override
+        public void onSubscribe(String channel) {
+
+        }
+
+        @Override
+        public void onMessage(String channel, String message) {
+
+        }
+
+        @Override
+        public void onUnsubscribe(String channel) {
+
+        }
     }
 }
